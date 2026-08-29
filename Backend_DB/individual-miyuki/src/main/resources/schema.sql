f0b308efc04ebded93b091fe75dd396813faa405
ak-miyuki <andreia.kubota@sptech.school>
2026-08-29 18:01:32 -0300

    apelido_viagem = unique

--- Backend_DB/individual-miyuki/src/main/resources/schema.sql
+++ Backend_DB/individual-miyuki/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS viagem (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    apelido_viagem VARCHAR(255) NOT NULL,
+    apelido_viagem VARCHAR(255) NOT NULL UNIQUE,
     estacao_do_ano VARCHAR(45) NOT NULL,
     data_ida DATE NOT NULL,
     data_volta DATE NOT NULL,
