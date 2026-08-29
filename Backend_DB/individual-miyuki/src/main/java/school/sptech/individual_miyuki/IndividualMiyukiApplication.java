efa788690779ed47d8bdb0efb3ade200e1466f4a
ak-miyuki <andreia.kubota@sptech.school>
2026-08-28 23:25:59 -0300

    projeto java criado no spring

---
+++ Backend_DB/individual-miyuki/src/main/java/school/sptech/individual_miyuki/IndividualMiyukiApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.individual_miyuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualMiyukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualMiyukiApplication.class, args);
+	}
+
+}
---
+++ Backend_DB/individual-miyuki/src/test/java/school/sptech/individual_miyuki/IndividualMiyukiApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.individual_miyuki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualMiyukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
