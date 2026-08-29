04ebe0044f4ac0479e95911b9a7cd6f8bf7fcf37
ak-miyuki <andreia.kubota@sptech.school>
2026-08-29 20:22:03 -0300

    Controller com get, get por apelido e post

--- Backend_DB/individual-miyuki/src/main/java/school/sptech/individual_miyuki/ViagemController.java
+++ Backend_DB/individual-miyuki/src/main/java/school/sptech/individual_miyuki/ViagemController.java
@@ -1,13 +1,15 @@
 package school.sptech.individual_miyuki;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.PreparedStatement;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -22,6 +24,8 @@ public class ViagemController {
     public ViagemController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    // Requests:
 
     @GetMapping
     public ResponseEntity<List<Viagem>> listarViagens() {
@@ -51,7 +55,76 @@ public class ViagemController {
         }
     }
 
+    @PostMapping
+    public ResponseEntity<Viagem> criarViagem(@RequestBody Viagem viagemParaCriar) {
 
+        String apelidoViagem = viagemParaCriar.getApelidoViagem();
+        String estacaoDoAno = viagemParaCriar.getEstacaoDoAno();
+        LocalDate dataIda = viagemParaCriar.getDataIda();
+        LocalDate dataVolta = viagemParaCriar.getDataVolta();
+        Integer diasNoJapao = viagemParaCriar.getDiasNoJapao();
+        Double reservaViagem = viagemParaCriar.getReservaViagem();
+
+        if (!viagemValidada(apelidoViagem, estacaoDoAno, dataIda, dataVolta, diasNoJapao, reservaViagem)) {
+            return ResponseEntity.status(400).build();
+        }
+
+        if (existeApelidoViagem(apelidoViagem)) {
+            return ResponseEntity.status(409).build();
+        }
+
+        String sql = "INSERT INTO viagem (apelido_viagem, estacao_do_ano, data_ida, data_volta, dias_no_japao, reserva_viagem) VALUES (?,?,?,?,?,?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql,
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, apelidoViagem);
+            ps.setString(2, estacaoDoAno);
+            ps.setObject(3, dataIda);
+            ps.setObject(4, dataVolta);
+            ps.setInt(5, diasNoJapao);
+            ps.setDouble(6, reservaViagem);
+
+            return ps;
+        }, keyHolder);
+
+        Integer idGerado = keyHolder.getKeyAs(Integer.class);
+
+        viagemParaCriar.setId(idGerado);
+
+        return ResponseEntity.status(201).body(viagemParaCriar);
+    }
+
+    // Validações:
+
+    public boolean existeApelidoViagem(String apelidoViagem) {
+
+        String sqlExisteApelidoViagem = "SELECT COUNT(*) FROM viagem WHERE UPPER(apelido_viagem) = ?";
+
+        Integer countId = jdbcTemplate.queryForObject(sqlExisteApelidoViagem,
+                Integer.class,
+                apelidoViagem.toUpperCase()
+        );
+
+        boolean existeApelidoViagem = countId >= 1;
+        return existeApelidoViagem;
+    }
+
+    public boolean viagemValidada(String apelidoViagem, String estacaoDoAno, LocalDate dataIda, LocalDate dataVolta, Integer diasNoJapao, Double reservaViagem) {
+
+        if (apelidoViagem == null || apelidoViagem.isBlank() ||
+                estacaoDoAno == null || estacaoDoAno.isBlank() ||
+                dataIda == null ||
+                dataVolta == null ||
+                diasNoJapao == null ||
+                reservaViagem == null) {
+
+            return false;
+        }
+        return true;
+    }
 
 
 }
